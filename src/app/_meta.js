e9683fcddfc289828c753c17a4cd8884d97562bd
Mikhail <mishapankin@gmail.com>
2025-06-14 00:41:58 +0300

    Basic site structure

---
+++ src/app/_meta.js
@@ -0,0 +1,21 @@
+const metaConfig = {
+    index: {
+        title: "Home",
+        display: "hidden",
+    },
+    docs: {
+        display: "children",
+    },
+    tutorial: {
+        title: "Tutorial",
+        href: "/docs/tutorial",
+        type: "page",
+    },
+    api: {
+        title: "API",
+        href: "/docs/api",
+        type: "page",
+    },
+}
+
+export default metaConfig;
---
+++ src/content/_meta.js
@@ -0,0 +1,16 @@
+const metaConfig = {
+    tutorial: {
+        title: "Tutorial",
+        type: "page"
+    },
+    api: {
+        title: "API",
+        type: "page",
+    },
+    donate: {
+        title: "Support us",
+        type: "page",
+    }
+}
+
+export default metaConfig;
