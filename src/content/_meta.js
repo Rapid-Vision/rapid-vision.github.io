e05b570366d285ee29f84a60d51f781ee9847024
Mikhail <mishapankin@gmail.com>
2025-06-14 00:41:58 +0300

    Init

---
+++ src/content/_meta.js
@@ -0,0 +1,12 @@
+const metaConfig = {
+    tutorial: {
+        title: "Tutorial",
+        type: "page"
+    },
+    api: {
+        title: "API",
+        type: "page",
+    }
+}
+
+export default metaConfig;
